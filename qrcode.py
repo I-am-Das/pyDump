52fb5041748d4abfcdc98357a815a248a5c1caf4
Deepak Arjun <deepakarjundas007@gmail.com>
2025-06-05 11:04:40 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -3,5 +3,5 @@ text="hello"
 r=qrcode.QRCode()
 r.add_data(text)
 r.make(fit=True)
-img=r.make_image{fill_color="Black",back_color="white"
+img=r.make_image(fill_color="Black",back_color="white")
 img.save("path")
